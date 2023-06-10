0d5b3d3c64c0359df6efe8cb0daa1d7b65f19cb2
Jrisani <jrisani@gmail.com>
2023-06-10 08:19:31 +0200

    Update ulohy - skuska

--- src/U01_premenne.java
+++ src/U01_premenne.java
@@ -12,7 +12,7 @@ public class U01_premenne {
         // Poznámka: Meno študenta, Dátum narodenia, známku a rok + aktuálny dátum nastavte tak,
         // aby sa dali meniť v zdrojovom kóde (t.j. cez premenné).
 
-        // skuska lamania textu asd asd v asdf v sasc casd asd as vsdf kl asalsk a sd a as ascasdav asf as fa v ascva sv casd v a sf cas d v as as c as d
+        // skuska lamania textu asd asd v asdf v sasc casd asd as vsdf kl asalsk a sd a as ascasdav asf as fa v ascva sv casd v a sf cas d v as as c as dwesdf sfdds fsd fsdfsdfsd fsdfsdfsdf sdf sdfsdf
 
         String menoStudenta = "Jozef Mrkvička";
         String datumNarodenia = ("03.04.2000");
@@ -22,7 +22,7 @@ public class U01_premenne {
         // naimportovanie triedy - ALT + ENTER - pre Date, SimpleDateFormat, LocalDate, Month
 
         //aktualny datum
-        Date datum = new Date();  // Date je trieda, ktora reprezentuje
+        Date datum = new Date();  // Date je OBJEKT, ktora reprezentuje
         //slovensky format
         SimpleDateFormat formatDatumu = new SimpleDateFormat("dd.MM.yyyy");
 
@@ -40,6 +40,7 @@ public class U01_premenne {
         //TODO Vypiste znamku vo formate, ktory je akceptovatelny pre Slovaka, cize zobrazit desatinnu ciarku.
         //% je na číslo
         // 2f naformatuje na dve desatine miesta
+        // %s - malé písmeno, %S - velké písmeno
         System.out.printf("Študent %s má známku %.1f", menoStudenta, znamka);
     }
 }
