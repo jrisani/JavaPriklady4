e3c92d6d116e491b0dfd4f6529d2e53362525e7d
Jrisani <jrisani@gmail.com>
2023-07-02 21:42:09 +0200

    Doplnenie ulohy zo 14. lekcie Aplikacia Kniznica, ktora sluzi na evidenciu knih. Ukazka ako pracovat s ArrayList, resp. ukladanie objektu Kniha do tohoto dynamickeho pola

---
+++ src/oop/kniznica/Kniha.java
@@ -0,0 +1,47 @@
+package oop.kniznica;
+
+public class Kniha {
+    private String nazov;
+    private String autor;
+    private int rokyVydania;
+
+    public Kniha(String nazov, String autor, int rovyVydania) {
+        this.nazov = nazov;
+        this.autor = autor;
+        this.rokyVydania = rovyVydania;
+    }
+
+    public String getNazov() {
+        return nazov;
+    }
+
+    public void setNazov(String nazov) {
+        this.nazov = nazov;
+    }
+
+    public String getAutor() {
+        return autor;
+    }
+
+    public void setAutor(String autor) {
+        this.autor = autor;
+    }
+
+    public int getRokyVydania() {
+        return rokyVydania;
+    }
+
+    public void setRokyVydania(int rokyVydania) {
+        this.rokyVydania = rokyVydania;
+    }
+
+
+    @Override
+    public String toString() {
+        return "Kniha{" +
+                "nazov='" + nazov + '\'' +
+                ", autor='" + autor + '\'' +
+                ", rokyVydania=" + rokyVydania +
+                '}';
+    }
+}
